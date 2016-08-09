85ddecb9b65318bc5d0ed5216e9e7672ecec720c
Denise Williams <williamsde@outlook.com>
2016-08-09 01:43:33 -0400

    added an about me section

--- helper.js
+++ helper.js
@@ -11,22 +11,22 @@ for(var i=0; i < skills_array.length; i += 1){
 
 //experience section
 
-var experience = '<li><h4>%data% &nbsp; &nbsp; &nbsp; ( %data1% ) </h4><h5>%data2%</h5></li>';
+var experience = '<li><h4>%data% &nbsp; &nbsp; &nbsp; ( %data1% ) </h4><br><h5>%data2%</h5></li><hr>';
 
 var experience_array = [
    {
        name:'Company Name' ,
-       years: 2010-2016, 
+       years: '2010-2016', 
        info:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
    },
    {
        name:'Company Name' ,
-       years:2008-2010, 
+       years: '2008-2010', 
        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
    },
    {
        name:'Company Name',
-       years: 2000-2008, 
+       years: '2000-2008', 
        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
    }
 ]
@@ -39,29 +39,33 @@ for (var i = 0; i < experience_array.length; i += 1) {
         'years']);
     experience_new = experience_new.replace('%data2%', experience_array[i]['info']);
     $('.experience-section-info').append(experience_new);
+
 }
 
 
 // projects section
-var projects = '<li><h3>...</h3><div class="row"><div class="col-md-3"><img src="./img/%data1%.gif" alt="project-images" class="img-responsive"></div><div class="col-md-9"><h3>Languages(s): %data2%</h3><h5>%data3%</h5></div></div></li><hr>'
+var projects = '<li><h3>...</h3><div class="row"><div class="col-md-3"><img src="./img/%data1%.gif" alt="project-images" class="img-responsive"></div><div class="col-md-9"><h4>Language(s): %data2%</h4><h5>Info: %data3%</h5><h5>Code: %data4%</h5></div></div></li><hr>'
 var projects_array = [
 {
-    name: '' ,
-    img: '',
-    language: '' ,
-    info:''
+    name:'Rock Paper Scissors',
+    img:'b',
+    language:'C#' ,
+    info:'The creation of this game involved making multple AIs (artificial intelligence bots) to respond in different ways to another players choice move.',
+    code:'https://github.com/MsBluphire/rock-paper-scissors'
     },
 {
     name: '',
     img: '',
     language: '',
-    info:''
+    info: '',
+    code:''
     },
 {
     name:'Unity Breakout Game',
     img: 'a',
     language: 'C#',
-    info:'A video game built using the Unity and C#.'
+    info: 'A video game built using the Unity and C#.',
+    code:''
     }
 ];
 
@@ -73,6 +77,7 @@ for(var i=0; i < projects_array.length; i += 1){
     ]);
     projects_new = projects_new.replace('%data2%', projects_array[i]['language']);
     projects_new = projects_new.replace('%data3%', projects_array[i]['info']);
+    projects_new = projects_new.replace('%data4%', projects_array[i]['code']);
     $('.projects-section-info').append(projects_new);
 
 }
