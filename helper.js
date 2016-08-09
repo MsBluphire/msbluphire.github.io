7f8b2fd2db14f385bc791a55c84d66eb908812cc
Denise Williams <williamsde@outlook.com>
2016-08-08 23:01:55 -0400

    Created Shell of projects section

--- helper.js
+++ helper.js
@@ -19,3 +19,37 @@ var experience_array = [
    {},
    {}
 ]
+
+var projects = '<li><h3>...</h3><div class="row"><div class="col-md-3"><img src="./img/%data1%.gif" alt="project-images" class="img-responsive"></div><div class="col-md-9"><h3>Languages(s): %data2%</h3><h5>%data3%</h5></div></div></li><hr>'
+var projects_array = [
+{
+    name: '' ,
+    img: '',
+    language: '' ,
+    info:''
+    },
+{
+    name: '',
+    img: '',
+    language: '',
+    info:''
+    },
+{
+    name: 'Unity Game App',
+    img: '',
+    language: '',
+    info:''
+    }
+];
+
+var projects_new = '';
+
+for(var i=0; i < projects_array.length; i += 1){
+    projects_new = projects.replace('%data%', projects_array[i]['name']);
+    projects_new = projects_new.replace('%data1%', projects_array[i]['img'
+    ]);
+    projects_new = projects_new.replace('%data2%', projects_array[i]['language']);
+    projects_new = projects_new.replace('%data3%', projects_array[i]['info']);
+    $('.projects-section-info').append(projects_new);
+
+}
