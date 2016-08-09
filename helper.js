426aa7c0906406e9e08ba5140027348a3cafc2f4
Denise Williams <williamsde@outlook.com>
2016-08-08 23:54:23 -0400

    All Skills showing on website now

--- helper.js
+++ helper.js
@@ -1,4 +1,4 @@
-﻿var skills = '<div class="col-md-3"><img src="./img/img1.png" alt="my skills" class="img-responsive img-circle"/></div>';
+﻿var skills = '<div class="col-md-3"><img src="./img/%data%.png" alt="my skills" class="img-responsive img-circle"/></div>';
 
 var skills_array = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12'];
 
@@ -7,7 +7,6 @@ var skills_new = '';
 for(var i=0; i < skills_array.length; i += 1){
     skills_new = skills.replace('%data%', skills_array[i]);
     $('.skills-section-images').append(skills_new);
-
 }
 
 //experience section
